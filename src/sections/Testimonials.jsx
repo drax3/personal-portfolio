c0015446da697cd2ede57ec88c92b83bf9cd4400
Tushar Kumar <tushar.kumar@solera.com>
2026-01-20 14:23:15 +0530

    experience

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,3 +1,18 @@
+const testimonials = [
+    {
+        quote: "Brave and smart",
+        author: "Hijimo Ippo",
+        role: "Boxer",
+        avatar: "",
+    },
+    {
+        quote: "Funny and good person",
+        author: "Sinchan Nohara",
+        role: "Kindergartener",
+        avatar: "",
+    },
+]
+
 export const Testimonials = () => {
     return <section>
     </section>
